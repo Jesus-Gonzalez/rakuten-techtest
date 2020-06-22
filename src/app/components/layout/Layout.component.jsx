bc013e55d0f72748ab7aa60ef99746c43d52faed
Jesus Gonzalez <jesus.gonzalez.eu@gmail.com>
2020-06-22 22:54:04 +0200

    feature/components/Layout: on click title goto home

--- src/app/components/layout/Layout.component.jsx
+++ src/app/components/layout/Layout.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from './Layout.styles'
 
@@ -9,7 +10,7 @@ const LayoutComponent = props => {
     <>
       <header className={styles.header}>
         <nav>
-          {title}
+          <Link to='/'>{title}</Link>
         </nav>
       </header>
       <main>
--- src/app/pages/home/useHomePage.hook.js
+++ src/app/pages/home/useHomePage.hook.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { compose } from 'redux'
 
-import { fetchFilms, resetFilms } from '~/core/store/actions'
+import {
+  fetchFilms,
+  resetFilms,
+  resetDetail
+} from '~/core/store/actions'
 
-export const useHomePage = configuration => {
+export const useHomePage = () => {
   const dispatch = useDispatch()
   const state = {
     application: useSelector(state => state.application),
     films: useSelector(state => state.films),
   }
   const actions = {
+    resetDetail: compose(dispatch, resetDetail),
     fetchFilms: compose(dispatch, fetchFilms),
     resetFilms: compose(dispatch, resetFilms),
   }
 
   React.useEffect(() => {
+    actions.resetDetail()
     actions.resetFilms()
 
     state.application.initialLists
